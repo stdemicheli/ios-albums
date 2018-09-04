7715322ececd638bb14d7aa63e6d0e2be9ec7a08
stdemicheli <stdemicheli@gmail.com>
2018-09-04 12:30:28 +0200

    Added storyboards.

---
+++ ios-albums/ios-albums/ViewControllers/SongTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SongTableViewCell.swift
+//  ios-albums
+//
+//  Created by De MicheliStefano on 04.09.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var songDurationTextField: UITextField!
+    
+    // MARK: - Methods
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+}
